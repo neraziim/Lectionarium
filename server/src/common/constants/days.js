f87e65b9b72d3c8db6edf38d9e3cc2a33437ef48
김성민 <neraziim@gmail.com>
2026-03-04 15:57:44 +0900

    chore: add constants for liturgical calendar logic including seasons and feast days

---
+++ server/src/common/constants/days.js
@@ -0,0 +1,4 @@
+export const WEEK_IN_DAY = 7;
+
+export const DAYS_TO_ASH = 46;
+export const EASTER_DAYS = 50;
---
+++ server/src/common/constants/liturgy.js
@@ -0,0 +1,19 @@
+//주일과 축일 독서의 주기
+export const ANNI_LECTIONARII = ['다해', '가해', '나해'];
+
+//전례력
+export const TEMPUS = {
+  ADVENTUS: '대림 시기',
+  NATIVITATIS: '성탄 시기',
+  QUADRAGESIMA: '사순 시기',
+  TRIDUUM_PASCHALE: '파스카 성삼일',
+  PASCHALE: '부활 시기',
+  ANNUM: '연중 시기',
+};
+
+//대축일
+export const SOLLEMNITAS = {
+  NATIVITAS_DOMINI: { month: 11, day: 25 },
+  DEI_GENETRICIS: { month: 0, day: 1 },
+  ASSUMTIO_MARIAE: { month: 7, day: 15 },
+};
